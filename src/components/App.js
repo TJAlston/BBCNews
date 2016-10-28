1a8ecbb8c8b35e2954f0e6d41b296ef11a34b390
TJAlston <tjalston82@gmail.com>
2016-10-28 18:13:04 -0400

    second bar complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import '../styles/screen.sass'
 import TopBar from './topBar'
+import SecondBar from './secondBar'
 
 class App extends Component {
 
   render () {
     return <div>
       <TopBar />
+      <SecondBar />
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
   }
--- src/components/secondBar.js
+++ src/components/secondBar.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import '../styles/screen.sass'
-import { Link } from 'react-router'
 
 class secondBar extends Component {
 
   render () {
     return <div>
-    <Link to='/'>HOME</Link>
+    <div className='titleBg'>
+      <div className='fixedWidth'>
+        <div className='pageTitle'>
+        <h1>NEWS</h1>
+        <h3 className='uk'>UK</h3>
+          <div className='rss'>
+          <h5>RSS</h5>
+          <img className='rsslogo' src={require('../images/rsslogo.png')} height='15' width='15' />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   }
 }
