b57729060a380b2de29fe20db8a5944e75fa1c13
TJAlston <tjalston82@gmail.com>
2016-10-29 11:16:41 -0400

    sidebars completed

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -7,6 +7,11 @@ class Watch extends Component {
     return <div>
     <div className='listen'>
       <h4>Watch/Listen</h4>
+      <img className='letter' src={require('../images/letter.png')} height='100' width='185' />
+      <img className='housing' src={require('../images/housing.png')} height='100' width='185' />
+        <div className='subtitle'>
+        <span className='live'>LIVE</span> <span className='BBCNews'>BBC News Channel</span>
+        </div>
     </div>
   </div>
   }
