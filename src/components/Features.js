d435eda3197ae899ce1f07f95f66e4e91356eefc
TJAlston <tjalston82@gmail.com>
2016-10-29 02:19:45 -0400

    features section completed

--- src/components/Features.js
+++ src/components/Features.js
@@ -10,12 +10,33 @@ class Features extends Component {
         <div className='sideStory'>
           <div className='cigs' />
             <div className='sideSubject'>
-          <h2>Toxic topic?</h2>
-          <p>How safe are e-cigarettes - and should they be banned?</p>
+              <h2>Toxic topic?</h2>
+              <p>How safe are e-cigarettes - and should they be banned?</p>
+            </div>
+          </div>
+          <div className='sideStory'>
+            <div className='colors' />
+            <div className='sideSubject'>
+              <h2>Counting colours</h2>
+              <p>How to reach the seven-a-day fruit and veg target</p>
+            </div>
+          </div>
+          <div className='sideStory'>
+            <div className='plane' />
+            <div className='sideSubject'>
+              <h2>Angle of descent</h2>
+              <p>The landscape bodering Heathrow Airport</p>
+            </div>
+          </div>
+          <div className='sideStory'>
+            <div className='history' />
+            <div className='sideSubject'>
+              <h2>Lessons from history</h2>
+              <p>Nixon, Reagan and Sarkozy's TV appearances</p>
+            </div>
+          </div>
         </div>
-      </div>
     </div>
-  </div>
   }
 }
 
